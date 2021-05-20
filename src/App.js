e900f86b09694c2641baca248208cbf557ab7565
Murad Cholukov <muradusa@gmail.com>
2021-05-20 07:21:34 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StreamChat } from "stream-chat";
 import {
   Chat,
@@ -19,29 +19,52 @@ chatClient.connectUser(
     id: "murad",
     name: "Murad Cholukov",
     image:
-      "https://getstream.io/random_png/?id=falling-poetry-4&name=falling-poetry-4",
+      "https://media-exp1.licdn.com/dms/image/C5603AQFNmzrc9sYQAA/profile-displayphoto-shrink_100_100/0/1618869151834?e=1625097600&v=beta&t=dnJbaZj4Xvi-EFOcDpLXxqifC0CQDwPWj2hdsR7zniw",
   },
   chatClient.devToken("murad")
 );
+
+const login = () => {
+  chatClient.connectUser(
+    {
+      id: "murad",
+      name: "Murad Cholukov",
+      image:
+        "https://media-exp1.licdn.com/dms/image/C5603AQFNmzrc9sYQAA/profile-displayphoto-shrink_100_100/0/1618869151834?e=1625097600&v=beta&t=dnJbaZj4Xvi-EFOcDpLXxqifC0CQDwPWj2hdsR7zniw",
+    },
+    chatClient.devToken("murad")
+  );
+};
+
+const logout = () => {
+  chatClient.disconnectUser();
+};
 
 const channel = chatClient.channel("messaging", "murad", {
   // add as many custom fields as you'd like
   image: "https://www.drupal.org/files/project-images/react.png",
   name: "Awesome Channel",
-  members: ["murad"],
+  members: ["murad", "jonh"],
 });
 
-const App = () => (
-  <Chat client={chatClient} theme="messaging light">
-    <Channel channel={channel}>
-      <Window>
-        <ChannelHeader />
-        <MessageList />
-        <MessageInput />
-      </Window>
-      <Thread />
-    </Channel>
-  </Chat>
-);
+const App = () => {
+  return (
+    <div>
+      <button onClick={login}>Login</button>
+      <button onClick={logout}>Log out</button>
+
+      <Chat client={chatClient} theme="messaging light">
+        <Channel channel={channel}>
+          <Window>
+            <ChannelHeader />
+            <MessageList />
+            <MessageInput />
+          </Window>
+          <Thread />
+        </Channel>
+      </Chat>
+    </div>
+  );
+};
 
 export default App;
